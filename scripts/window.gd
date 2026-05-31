8aa8fca061631ed4b2eea51df495409332f09d9b
Steven Raines <stevenmraines@gmail.com>
2026-05-31 18:37:33 -0400

    I don't even know.

--- scripts/window.gd
+++ scripts/window.gd
@@ -4,6 +4,7 @@ extends Control
 
 @onready var yt_dlp_path_input := $Panel/MarginContainer/VSplitContainer/YtDlpConfig/HBoxContainer/MarginContainer/LineEdit
 @onready var yt_dlp_input_timer := $YtDlpInputTimer
+@onready var channel_container := $Panel/MarginContainer/VSplitContainer/HBoxContainer/ChannelContainer
 @onready var console_text_input := $Panel/MarginContainer/VSplitContainer/MarginContainer/Console/MarginContainer/ConsoleText
 
 var typing := false
@@ -14,12 +15,31 @@ func _unhandled_input(event: InputEvent) -> void:
 		get_viewport().set_input_as_handled()
 		get_tree().quit(0)
 	
-	if yt_dlp_path_input.has_focus():
-		yt_dlp_path = yt_dlp_path_input.text
-		get_viewport().set_input_as_handled()
-		typing = true
-		yt_dlp_input_timer.start()
+	
+	if event is InputEventKey:
+		if yt_dlp_path_input.has_focus():
+			yt_dlp_path = yt_dlp_path_input.text
+			get_viewport().set_input_as_handled()
+			typing = true
+			yt_dlp_input_timer.start()
+			return
+		
+		for channel in channel_container.get_children():
+			for playlist in channel.playlist_container.get_children():
+				if playlist.backup_path_input.has_focus():
+					write_to_console(playlist.backup_path_input.text)
+					get_viewport().set_input_as_handled()
+				elif playlist.remote_path_input.has_focus():
+					write_to_console(playlist.remote_path_input.text)
+					get_viewport().set_input_as_handled()
 
 
 func _on_yt_dlp_input_timer_timeout() -> void:
-	console_text_input.text = console_text_input.text + "write to file " + yt_dlp_path + "\n"
+	write_to_console("write to file " + yt_dlp_path)
+
+
+func write_to_console(text : String) -> void:
+	console_text_input.text = console_text_input.text + text + "\n"
+	var total_lines = console_text_input.get_line_count()
+	console_text_input.set_caret_line(total_lines - 1)
+	console_text_input.set_caret_column(console_text_input.get_line_width(total_lines - 1))
